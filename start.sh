007711d24d5d489b66b6e39f8c0c3adff8e10ae6
Finn K <fyndalf@users.noreply.github.com>
2019-05-25 15:20:21 +0200

    Update start script to always set secret key in granary node

--- start.sh
+++ start.sh
@@ -58,7 +58,7 @@ elif [ "$1" == "sandbox" ]; then
     granary init
     sudo chmod -R 777 ./.granary
     granary node start&
-    granary client - import secret key "activator" "unencrypted:edsk31vznjHSSpGExDMHYASz45VZqXN4DPxvsa4hAyY8dHM28cZzp6"
+    granary client - import secret key "activator" "unencrypted:edsk31vznjHSSpGExDMHYASz45VZqXN4DPxvsa4hAyY8dHM28cZzp6" --force
     granary client - "--block genesis activate protocol ProtoALphaALphaALphaALphaALphaALphaALphaALphaDdp3zK with fitness 1 and key activator and parameters $PWD/protocol_parameters.json --timestamp $(TZ='AAA+1' date +%FT%TZ)" 
     granary client - import secret key "bootstrap1" "unencrypted:edsk3gUfUPyBSfrS9CCgmCiQsTCHGkviBDusMxDJstFtojtc1zcpsh"
 else
