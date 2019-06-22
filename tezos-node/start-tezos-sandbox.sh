fdc2bb3707ad3d812d2eb1399a5744db915109d1
Finn K <fyndalf@users.noreply.github.com>
2019-06-22 15:11:47 +0200

    Possibly fix alpha activation

--- tezos-node/start-tezos-sandbox.sh
+++ tezos-node/start-tezos-sandbox.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
-
+shopt -s expand_aliases
+echo "-> starting sandboxed node!"
 DATA_DIR='/var/run/tezos/node' /tezos/src/bin_node/tezos-sandboxed-node.sh 1 --connections 1 &
 P1=$!
-eval 'sleep 5 && eval '/tezos/src/bin_client/tezos-init-sandboxed-client.sh 1'' &
+
+eval 'sleep 5' &
 P2=$!
 wait $P2
-cat /tezos/src/bin_client//../../scripts/protocol_parameters.json
-/tezos/src/bin_client//../../_build/default/src/bin_client/main_client.exe -base-dir /var/run/tezos/client -addr localhost -port 18731 -block genesis activate protocol Pt24m4xiPbLDhVgVfABUjirbmda3yohdN82Sp9FeuAXJ4eV9otd with fitness 1 and key activator and parameters /tezos/src/bin_client//../../scripts/protocol_parameters.json --timestamp 2019-06-21T11:20:44Z
-#/tezos/src/bin_client//../../_build/default/src/bin_client/main_client.exe -addr localhost -port 18731 rpc get /chains/main/blocks/head/metadata
-#/tezos/src/bin_client//../../_build/default/src/bin_client/main_client.exe -addr localhost -port 18731 list known addresses
+
+echo "-> initialising sandboxed client!"
+eval "$(/tezos/src/bin_client/tezos-init-sandboxed-client.sh 1)"
+
+echo "-> activate alpha!"
+tezos-activate-alpha
+
+echo "-> activated chains:"
+/tezos/src/bin_client//../../_build/default/src/bin_client/main_client.exe -addr localhost -port 18731 rpc get /chains/main/blocks/head/metadata
+
+echo "-> known addresses:"
+/tezos/src/bin_client//../../_build/default/src/bin_client/main_client.exe -addr localhost -port 18731 list known addresses
 wait $P1
