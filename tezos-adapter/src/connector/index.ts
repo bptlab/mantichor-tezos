537a5f20cdf438ad46d5244c58aeb74673da1fe3
Finn K <fyndalf@users.noreply.github.com>
2019-06-22 15:20:33 +0200

    Fix indentation and add todo

--- tezos-adapter/src/connector/index.ts
+++ tezos-adapter/src/connector/index.ts
@@ -3,10 +3,11 @@ import Sotez, { crypto, forge, ledger, utility } from '../../node_modules/sotez/
 
 const key = 'edsk3gUfUPyBSfrS9CCgmCiQsTCHGkviBDusMxDJstFtojtc1zcpsh';
 const sotez = new Sotez('http://127.0.0.1:18731', 'main', 'main');
+// todo: originate account for this adapter
 
 const importKey = async () => {
     await sotez.importKey(key).then(console.log('Imported key.'))
-    .catch((err) => console.error('Error importing key:', err));
+        .catch((err) => console.error('Error importing key:', err));
 };
 
 // todo: make functions return information
@@ -17,7 +18,7 @@ export async function deployContract(code: string, balance: number = 0, init: st
         amount: '7',
         to: 'tz1gjaF81ZRRvdzjobyfVNsAeSC6PScjfQwN',
     }).then((res) => console.log('Transfer result:', res))
-    .catch((err) => console.error('Error sending transaction:', err));
+        .catch((err) => console.error('Error sending transaction:', err));
     // console.log(code, balance, init);
 
     // origination does not seem to work
