12a94b6650048406089f527e2736cc6701bfc3d4
Finn K <fyndalf@users.noreply.github.com>
2019-08-21 19:12:25 +0200

    Move Adapter from docker-compose to own dockerfile

--- reset.sh
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env bash
-docker volume rm mantichor-tezos_client_data
-docker volume rm mantichor-tezos_node_data
--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
 #! /usr/bin/env bash
-docker-compose up --build --detach
+docker build -f docker-tezos-adapter.yaml . -t mantichor-tezos-adapter
+docker run -p 7320:7320 --name mantichor-tezos-adapter -d mantichor-tezos-adapter
+docker logs -f mantichor-tezos-adapter
